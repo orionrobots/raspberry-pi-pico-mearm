01c66974af5f9618a3a32af2627e09eec3a08afe
Danny Staple <danny@orionrobots.co.uk>
2023-05-11 16:25:37 +0100

    Step 1 arm control code

--- code/mearm.py
+++ code/mearm.py
@@ -1,72 +1,53 @@
 """
 import sys
 del sys.modules["mearm"]
-import mearm
-arm = mearm.SmoothArm()
+from mearm import Arm
 # tests
-arm.move(120, 150, 50)
-arm.reset()
-arm.gripper_position(mearm.grip_closed)
-arm.gripper_position(mearm.grip_fully_open)
+Arm.grip.move(120)
+Arm.grip.reset()
+Arm.base.move(120)
+Arm.base.reset()
+Arm.left.move(120)
+Arm.left.reset()
+Arm.right.move(120)
+Arm.right.reset()
 """
 import machine
 import utime
 
-def degrees_to_duty(angle):
-    return int(1000 + (angle * 8000/180))
+PWM_MIN = 1000
+PWM_MAX = 9000
+PWM_RANGE = PWM_MAX - PWM_MIN
+PWM_FREQ = 50
 
+class Servo:
+    def __init__(self, pin, reset_position=90, min_position=0, max_position=180):
+        self.pwm = machine.PWM(machine.Pin(pin, machine.Pin.OUT))
+        self.pwm.freq(PWM_FREQ)
+        self.current = self.reset_position = reset_position
+        self.max_position = max_position
+        self.min_position = min_position
 
-def set_angle(servo, angle):
-    servo.duty_u16(degrees_to_duty(angle))
+    def degrees_to_duty(self, angle):
+        return int(PWM_MIN + (angle * PWM_RANGE/180))
 
-grip_closed = 157
-grip_fully_open = 100
+    def set_angle(self, angle):
+        self.pwm.duty_u16(self.degrees_to_duty(angle))
 
-class SmoothArm:
-    left_current = 90
-    right_current = 90 # right servo got hot? did I go too high - or is it a broken servo?
-    base_current = 90
-    grip_current = 100
-    
-    left_lower = 30
-    left_upper = 120
-    right_lower = 50
-    right_upper = 150
-    
-    def __init__(self):
-        self.grip_servo = machine.PWM(machine.Pin(4, machine.Pin.OUT))
-        self.left_servo = machine.PWM(machine.Pin(5, machine.Pin.OUT))
-        self.right_servo = machine.PWM(machine.Pin(6, machine.Pin.OUT))
-        self.base_servo = machine.PWM(machine.Pin(7, machine.Pin.OUT))
-        self.left_servo.freq(50)
-        self.grip_servo.freq(50)
-        self.right_servo.freq(50)
-        self.base_servo.freq(50)      
-    
-    def move(self, left_pos, right_pos, base_pos, seconds=1, steps=100):
+    def move(self, position, seconds=1, steps=100):
         step_time = seconds/steps
-        left_step = (left_pos - self.left_current)/steps
-        right_step = (right_pos - self.right_current)/steps
-        base_step = (base_pos - self.base_current)/steps
-        
+        position = max(self.min_position, min(self.max_position, position))
+        step_size = (position - self.current) / steps
         for n in range(steps):
-            set_angle(self.left_servo, self.left_current)
-            set_angle(self.right_servo, self.right_current)
-            set_angle(self.base_servo, self.base_current)
-            self.left_current += left_step
-            self.right_current += right_step
-            self.base_current += base_step
-            utime.sleep(seconds/steps)
-    
-    def gripper_position(self, position, seconds=1, steps=100):
-        step_time = seconds/steps
-        grip_step = (position - self.grip_current) / steps
-        
-        for n in range(steps):
-            set_angle(self.grip_servo, self.grip_current)
-            self.grip_current += grip_step
-            utime.sleep(seconds/steps)
+            utime.sleep(step_time)
+            self.set_angle(self.current)
+            self.current += step_size
 
-    def reset(self):
-        self.move(90, 90, 90)
-        self.gripper_position(grip_fully_open)
+    def reset(self, seconds=1, steps=100):
+        self.move(self.reset_position, seconds=seconds, steps=steps)
+
+class Arm:
+    grip_servo = Servo(4, reset_position=100, min_position=100, max_position=157)
+    left_servo = Servo(5, min_position=30, max_position=120)
+    right_servo = Servo(6, min_position=50, max_position=150)
+    base_servo = Servo(7, min_position=30, max_position=150)
