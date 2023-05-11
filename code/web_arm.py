09abbd27190829883473c89873eb296d76d415a3
Danny Staple <danny@orionrobots.co.uk>
2023-05-11 16:32:05 +0100

    Add elbow and shoulder sliders

--- code/web_arm.py
+++ code/web_arm.py
@@ -26,6 +26,16 @@ async def _set_base(request, position):
     uasyncio.create_task(Arm.base_servo.move(position))
     return 'Moving'
 
+@app.route('/set_shoulder/<int:position>')
+async def _set_shoulder(request, position):
+    uasyncio.create_task(Arm.shoulder_servo.move(position))
+    return 'Moving'
+
+@app.route('/set_elbow/<int:position>')
+async def _set_elbow(request, position):
+    uasyncio.create_task(Arm.elbow_servo.move(position))
+    return 'Moving'
+
 try:
     wifi_connect()
     print("Starting app")
