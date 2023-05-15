55f4139743903ed4dcdbe63667b4756b23cbf3e0
Danny Staple <danny@orionrobots.co.uk>
2023-05-15 14:09:43 +0100

    Backport suitable changes from IK step

--- code/mearm.py
+++ code/mearm.py
@@ -5,17 +5,18 @@ from mearm import Arm
 # tests
 import uasyncio
 
+arm = mearm.Arm()
 tasks = [
-    uasyncio.create_task(Arm.grip.move(120)),
-    uasyncio.create_task(Arm.base.move(120)),
-    uasyncio.create_task(Arm.shoulder.move(120)),
-    uasyncio.create_task(Arm.elbow.move(120)),
+    uasyncio.create_task(arm.grip.move(120)),
+    uasyncio.create_task(arm.base.move(120)),
+    uasyncio.create_task(arm.shoulder.move(120)),
+    uasyncio.create_task(arm.elbow.move(120)),
 ]
 uasyncio.run(uasyncio.gather(*tasks))
 # or
 
-uasyncio.run(Arm.move_together(base_angle=120, shoulder_angle=120, elbow_angle=120, grip_angle=120))
-uasyncio.run(Arm.reset())
+uasyncio.run(arm.move_together(base_angle=120, shoulder_angle=120, elbow_angle=120, grip_angle=120))
+uasyncio.run(arm.reset())
 """
 
 import machine
@@ -25,6 +26,7 @@ PWM_MIN = 1000
 PWM_MAX = 9000
 PWM_RANGE = PWM_MAX - PWM_MIN
 PWM_FREQ = 50
+DEGREES_TO_PWM = PWM_RANGE / 180
 
 class AsyncServo:
     def __init__(self, pin, reset_position=90, min_position=0, max_position=180):
@@ -35,7 +37,7 @@ class AsyncServo:
         self.min_position = min_position
 
     def degrees_to_duty(self, angle):
-        return int(PWM_MIN + (angle * PWM_RANGE/180))
+        return int(PWM_MIN + (angle * DEGREES_TO_PWM))
 
     def set_angle(self, angle):
         self.pwm.duty_u16(self.degrees_to_duty(angle))
@@ -53,22 +55,23 @@ class AsyncServo:
         await self.move(self.reset_position, seconds=seconds, steps=steps)
 
 class Arm:
-    grip = AsyncServo(4, reset_position=100, min_position=100, max_position=157)
-    shoulder = AsyncServo(5, min_position=30, max_position=120)
-    elbow = AsyncServo(6, min_position=50, max_position=150)
-    base = AsyncServo(7, min_position=30, max_position=150)
+    def __init__(self, grip_pin=4, elbow_pin=5, shoulder_pin=6, base_pin=7):
+        self.grip = AsyncServo(grip_pin, reset_position=100, min_position=100, max_position=157)
+        self.elbow = AsyncServo(elbow_pin, min_position=0, max_position=150)
+        self.shoulder = AsyncServo(shoulder_pin, min_position=0, max_position=145)
+        self.base = AsyncServo(base_pin, min_position=30, max_position=150)
 
-    async def move_together(base_angle=None, shoulder_angle=None, elbow_angle=None, grip_angle=None, seconds=1, steps=100):
+    async def move_together(self, base_angle=None, shoulder_angle=None, elbow_angle=None, grip_angle=None, seconds=1, steps=100):
         tasks = []
         if base_angle is not None:
-            tasks.append(uasyncio.create_task(Arm.base.move(base_angle, seconds=seconds, steps=steps)))
+            tasks.append(uasyncio.create_task(self.base.move(base_angle, seconds=seconds, steps=steps)))
         if shoulder_angle is not None:
-            tasks.append(uasyncio.create_task(Arm.shoulder.move(shoulder_angle, seconds=seconds, steps=steps)))
+            tasks.append(uasyncio.create_task(self.shoulder.move(shoulder_angle, seconds=seconds, steps=steps)))
         if elbow_angle is not None:
-            tasks.append(uasyncio.create_task(Arm.elbow.move(elbow_angle, seconds=seconds, steps=steps)))
+            tasks.append(uasyncio.create_task(self.elbow.move(elbow_angle, seconds=seconds, steps=steps)))
         if grip_angle is not None:
-            tasks.append(uasyncio.create_task(Arm.grip.move(grip_angle, seconds=seconds, steps=steps)))
+            tasks.append(uasyncio.create_task(self.grip.move(grip_angle, seconds=seconds, steps=steps)))
         await uasyncio.gather(*tasks)
 
-    async def reset(seconds=1, steps=100):
-        await Arm.move_together(base_angle=90, shoulder_angle=90, elbow_angle=90, grip_angle=100, seconds=seconds, steps=steps)
+    async def reset(self, seconds=1, steps=100):
+        await self.move_together(base_angle=90, shoulder_angle=90, elbow_angle=90, grip_angle=100, seconds=seconds, steps=steps)
--- code/web_arm.py
+++ code/web_arm.py
@@ -1,9 +1,12 @@
+import machine
+import uasyncio
+
 from microdot_asyncio import Microdot
 from connect import wifi_connect
 from mearm import Arm
-import uasyncio
 
 app = Microdot()
+arm = Arm()
 
 @app.route('/')
 async def index(request):
@@ -11,29 +14,24 @@ async def index(request):
     with open("control.html") as content:
         return content.read(), 200, {'Content-Type': 'text/html'}
 
-@app.route('/open')
-async def _open(request):
-    uasyncio.create_task(Arm.grip.move(Arm.grip.min_position))
-    return 'Opening'
-    
-@app.route('/close')
-async def _close(request):
-    uasyncio.create_task(Arm.grip.move(Arm.grip.max_position))
-    return 'Closing'
+@app.route('/set_grip/<int:position>')
+async def _set_base(request, position):
+    uasyncio.create_task(arm.grip.move(position))
+    return 'Moving'
 
 @app.route('/set_base/<int:position>')
 async def _set_base(request, position):
-    uasyncio.create_task(Arm.base.move(position))
+    uasyncio.create_task(arm.base.move(position))
     return 'Moving'
 
 @app.route('/set_shoulder/<int:position>')
 async def _set_shoulder(request, position):
-    uasyncio.create_task(Arm.shoulder.move(position))
+    uasyncio.create_task(arm.shoulder.move(position))
     return 'Moving'
 
 @app.route('/set_elbow/<int:position>')
 async def _set_elbow(request, position):
-    uasyncio.create_task(Arm.elbow.move(position))
+    uasyncio.create_task(arm.elbow.move(position))
     return 'Moving'
 
 try:
